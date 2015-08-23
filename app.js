e8392f004781efab933c2d15d35e0e1ad892d99f
Josh <josh@joshglazebrook.com>
2015-08-22 20:11:30 -0700

    changed output

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express();
 app.get('/', function (req, res) {
-  var ob = {};
+  var obj = {};
   obj.node = process.env.TUTUM_NODE_HOSTNAME;
-  obj.version = "v2";
+  
+
+  res.send(obj);
 
   //res.send(process.env.TUTUM_NODE_HOSTNAME);
 });
