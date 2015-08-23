6619408a1087ba4e061debd926b771b7150d6163
Josh <josh@joshglazebrook.com>
2015-08-22 22:19:59 -0700

    added loaderio

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var app = express();
 app.get('/', function (req, res) {
   var obj = {};
   obj.node = process.env.TUTUM_NODE_HOSTNAME;
-  
+
 
   res.send(obj);
 
   //res.send(process.env.TUTUM_NODE_HOSTNAME);
+});
+
+app.get('e8d2ee8c760dba091e33a7bfe767c341.txt', function (req, res) {
+  res.send('loaderio-e8d2ee8c760dba091e33a7bfe767c341');
 });
 
 app.listen(process.env.PORT);
