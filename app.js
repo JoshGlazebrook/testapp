25c2833340586742e6a34d76f3e0f77dc9c4f436
Josh <josh@joshglazebrook.com>
2015-08-24 16:15:25 -0700

    aded logging

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
+var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var app = express();
+app.use(logger('dev'));
+app.use(methodOverride());
+
+
 app.get('/', function (req, res) {
   var obj = {};
   obj.node = process.env.TUTUM_NODE_HOSTNAME;
@@ -15,4 +21,8 @@ app.get('/loaderio-e8d2ee8c760dba091e33a7bfe767c341.txt', function (req, res) {
   res.send('loaderio-e8d2ee8c760dba091e33a7bfe767c341');
 });
 
-app.listen(process.env.PORT);
+app.get('/test', function (req, res) {
+  res.send('pass');
+});
+
+app.listen(process.env.PORT || 3000);
